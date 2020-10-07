718011b321e7abe12318e85c48e1175793dd0027
Rashed Khan <abir97bd@gmail.com>
2020-10-07 12:57:31 +0600

    Updates

---
+++ precache-manifest.d93e8df885f0b4cdd6ab06f2dfa7fa43.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eaef314f5291af88b50f24d55b0840c",
+    "url": "/Netflix/index.html"
+  },
+  {
+    "revision": "ea2521603c23281734f8",
+    "url": "/Netflix/static/css/main.0417e945.chunk.css"
+  },
+  {
+    "revision": "60be772fb89fe41255b0",
+    "url": "/Netflix/static/js/2.f33c0f98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Netflix/static/js/2.f33c0f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2521603c23281734f8",
+    "url": "/Netflix/static/js/main.8fcc510e.chunk.js"
+  },
+  {
+    "revision": "1111f8ae901435215b7d",
+    "url": "/Netflix/static/js/runtime-main.17f22a80.js"
+  },
+  {
+    "revision": "a1de92e803dd76633e92796aae3333c7",
+    "url": "/Netflix/static/media/dark.a1de92e8.png"
+  },
+  {
+    "revision": "fb413d5ce9bfe988fddd570c67c21df4",
+    "url": "/Netflix/static/media/im1.fb413d5c.jpg"
+  },
+  {
+    "revision": "dbdae22a034791aaac3ab0f6faefb029",
+    "url": "/Netflix/static/media/im2.dbdae22a.jpg"
+  },
+  {
+    "revision": "e57d150d258e615160054e41c962b7d9",
+    "url": "/Netflix/static/media/im3.e57d150d.jpg"
+  }
+]);
