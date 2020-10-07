a304a6c914e63922812f1fb71faa34cdda161c0c
Rashed Khan <abir97bd@gmail.com>
2020-10-07 12:56:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cards">
+        <div className="card">
+          <img src={props.img} />
+          <div>
+            <span>{props.span}</span>
+          </div>
+          <h2>{props.name}</h2>
+          <button><a href={props.link}>Watch Now</a></button>
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import im2 from "./img/im2.jpg";
+import im1 from "./img/im1.jpg";
+import im3 from "./img/im3.jpg";
+import dark from "./img/dark.png";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <header>
+      <a className="header">NETFLIX</a>
+    </header>
+    <App
+      img={im1}
+      link="https://drive.google.com/drive/u/2/folders/1jHo5q3bZfqEgUvjiWoWXPsFExLoy_8n8"
+      name="Stranger Things Season-1"
+      span="A netflix original series"
+    />
+    <App img={dark} name="Dark Season-1" span="A netflix original series" />
+    <App
+      img={im2}
+      link="https://drive.google.com/drive/u/2/folders/1ECvdrVB7i3gfhhU8D-PGlY5CUSKY3Skq"
+      name="Stranger Things Season-2"
+      span="A netflix original series"
+    />
+    <App img={dark} name="Dark Season-1" span="A netflix original series" />
+    <App
+      img={im3}
+      link="https://drive.google.com/drive/u/2/folders/1Uw-vQS5q8QK-Bs7sv4Nm0KfyYm14Adta"
+      name="Stranger Things Season-3"
+      span="A netflix original series"
+    />
+    <App img={dark} name="Dark Season-1" span="A netflix original series" />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
